13af37fcb787fb02a0477ef7dd58a576b5aafaf0
montipallarols <montaga.pallarols@gmail.com>
2021-01-06 19:17:40 +0100

    Create signup endpoint

--- routers/auth.js
+++ routers/auth.js
@@ -1,6 +1,7 @@
 const { Router } = require('express')
 const { toJWT, toData } = require('../auth/jwt')
 const bcrypt = require('bcrypt');
+const { SALT_ROUNDS } = require("../config/constants");
 const User = require("../models").user;
 const router = new Router()
 const authMiddleware = require("../auth/middleware");
@@ -29,6 +30,38 @@ router.post("/login", async (req, res, next) => {
     return res.status(200).send({ token, ...user.dataValues });
   } catch (error) {
     console.log(error);
+    return res.status(400).send({ message: "Something went wrong, sorry" });
+  }
+});
+
+router.post("/signup", async (req, res) => {
+  const { fullName, email, password, address } = req.body;
+  if (!fullName || !email || !password || !address) {
+    return res.status(400).send("Please provide your full name, email, password and address");
+  }
+
+  try {
+    const newUser = await User.create({
+      fullName,
+      email,
+      password: bcrypt.hashSync(password, SALT_ROUNDS),
+      address
+    });
+    console.log("do I get HERE?")
+    console.log("----new user----", newUser)
+
+    delete newUser.dataValues["password"]; // don't send back the password hash
+
+    const token = toJWT({ userId: newUser.id });
+
+    res.status(201).json({ token, ...newUser.dataValues });
+  } catch (error) {
+    if (error.name === "SequelizeUniqueConstraintError") {
+      return res
+        .status(400)
+        .send({ message: "There is an existing account with this email" });
+    }
+
     return res.status(400).send({ message: "Something went wrong, sorry" });
   }
 });
